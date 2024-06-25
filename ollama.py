9fdb31a524d705412b4af5989e367b4bc9df09fe
sejon <wkdtpwhs@gmail.com>
2024-06-25 22:46:03 +0900

    llm 번역 프롬프트 수정

--- ollama.py
+++ ollama.py
@@ -26,10 +26,19 @@ example_prompt = PromptTemplate.from_template(
 )
 
 ## llm 사용을 위한 프롬프트
+## lang, contents 는 모두 langchain 에서 변수로 받아올 수 있다
 prompt = FewShotPromptTemplate(
     example_prompt=example_prompt,
     examples=examples,
-    suffix="Translate my sentence into {lang} : {contents}", ## lang, contents 는 모두 langchain 에서 변수로 받아올 수 있다
+    suffix=""" 
+    1. 사용자가 제공한 {contents}를 {lang}에 해당하는 언어로 번역합니다.
+    2. 번역된 내용을 분석하여 각 문단의 핵심 주제를 나타내는 대표적인 해시태그를 추출합니다.
+    3. 번역된 내용과 해시태그를 조합하여 최종 결과를 사용자에게 제공합니다.
+    - 예) 
+        번역된 내용: 챗GPT는 인공지능 기술의 발전으로 만들어진 대화형 AI 모델입니다. 이를 통해 사용자는 다양한 질문에 대한 답변을 얻을 수 있습니다.
+    해시태그:
+        #chatgpt #ai #it
+    """, 
     input_variables=["lang", "contents"]
 )
 
